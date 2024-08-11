93305f14638916b9251b4915900ed47e3af6de6b
KavishkaSasindu <sasindu0215@gmail.com>
2024-08-11 21:16:18 +0530

    initialize commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program{
+    public static void Main(string[] args){
+
+        int age = 23;
+
+        Console.WriteLine(age);
+
+        Console.WriteLine(int.MaxValue);
+        Console.WriteLine(int.MinValue);
+
+        Console.WriteLine(long.MaxValue);
+        Console.WriteLine(long.MinValue);
+    }
+}
