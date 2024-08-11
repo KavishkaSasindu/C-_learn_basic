a83b9d7308006751501162e0deeb25f0481c0d36
KavishkaSasindu <sasindu0215@gmail.com>
2024-08-11 21:39:56 +0530

    updated by ksdev

--- Program.cs
+++ Program.cs
@@ -3,14 +3,35 @@
 class Program{
     public static void Main(string[] args){
 
-        int age = 23;
+        // Console.WriteLine(age);
 
-        Console.WriteLine(age);
+        // Console.WriteLine(int.MaxValue);
+        // Console.WriteLine(int.MinValue);
 
-        Console.WriteLine(int.MaxValue);
-        Console.WriteLine(int.MinValue);
+        // Console.WriteLine(long.MaxValue);
+        // Console.WriteLine(long.MinValue);
 
-        Console.WriteLine(long.MaxValue);
-        Console.WriteLine(long.MinValue);
+        // converting string into numbers
+
+        // string number = "23";
+
+        // int age = Convert.ToInt32(number);
+        // Console.WriteLine(age);
+
+        // int number = 22;
+
+        // if(number%2 == 0){
+        //     Console.WriteLine("Number is even number");
+        // }else{
+        //     Console.WriteLine("Number is odd number");
+        // }
+
+        // var keyword
+        /*using var keyword when the variable initialize using var the compiler will automatically updated the data type what we assign*/
+
+        var name = "Kavishka Sasindu";
+        Console.WriteLine(name);
+
+
     }
 }
--- obj/Debug/net8.0/myc#.AssemblyInfo.cs
+++ obj/Debug/net8.0/myc#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myc#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93305f14638916b9251b4915900ed47e3af6de6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("myc#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myc#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
